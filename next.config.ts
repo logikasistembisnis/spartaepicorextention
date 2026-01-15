afebb8002730649c25e85f8539965e514d6f603d
nelifzyh <nelifauziyah07@gmail.com>
2026-01-15 09:02:16 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // domains: ["qrcode.tec-it.com"],
+  },
 };
 
 export default nextConfig;
