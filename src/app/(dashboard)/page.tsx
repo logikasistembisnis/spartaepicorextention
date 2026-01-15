3e275edbe548cadf8c100ea5c91b49ab9c434135
nelifzyh <nelifauziyah07@gmail.com>
2026-01-15 09:04:51 +0700

    dashboard

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="text-gray-900 text-xl md:text-2xl font-semibold">Selamat Datang di Sparta Epicor Extention</p>
+    </div>
+  );
+}
---
+++ src/components/DashboardWrapper.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useSidebar } from '../context/SidebarContext'
+
+export default function DashboardWrapper({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { isOpen } = useSidebar()
+
+  return (
+    // Main Content yang margin-kirinya berubah dinamis
+    <main 
+        className={`pt-16 min-h-screen transition-all duration-300 ease-in-out ${
+            isOpen ? 'ml-0 lg:ml-64' : 'ml-0'
+        }`}
+    >
+        <div className="p-4 sm:p-8">
+            {children}
+        </div>
+    </main>
+  )
+}
